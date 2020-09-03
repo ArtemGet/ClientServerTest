1dd85095012e8c41a0282a6abb3ea413c633fc40
Artem Getmanskii Win <re910@yandex.ru>
2020-09-03 16:31:03 +0300

    add multiThread

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -8,95 +8,104 @@ import java.util.ArrayList;
 public class Server {
     public static void main(String[] args) {
 
-        try (ServerSocket server = new ServerSocket(9000);) {
+        try (ServerSocket server = new ServerSocket(9000)) {
             System.out.println("Start");
             while (true) {
-                try (RW rW = new RW(server)) {
-                    int Id;
-                    int key;
-                    DBAccess a;
-                    switch (rW.readLine()) {
-//Registration Android Client
-                        case "userRegData" :
-                            a = new DBAccess();
-                            Object[] userData = rW.readUserData();
-                            System.out.println("connected");
-                            a.setUserData((String) userData[0],(String)userData[1], (int)userData[2]);
-                            System.out.println(a.getId((String) userData[0],(String) userData[1],(int)userData[2]));
-                            //добавить инд ключ для id
-                            rW.write(a.getId((String) userData[0],(String) userData[1],(int)userData[2]));
-                            break;
-//Adding key to Android Client after verification
-                        case "userId":
-                            Id = rW.read();
-                             a = new DBAccess();
-                             key = a.checkKey(Id);
-                            if (key != 0) {
-                                key = Gen.genKey();
-                                rW.write(key);
-                            }
-                            else {
-                                rW.close();
-                            }
-                            break;
-                        //Verify Android Client
-                        case "adminId":
-                            System.out.println("connected");
-                            a = new DBAccess();
-                            ArrayList<Integer> ID = a.getUnverifiedId();
-                            ArrayList<String> DATA =  a.getUnverifiedData();
-                            rW.writeUnverified(ID,DATA);
-                            String respond = rW.readLine();
-                            System.out.println(respond);
 
-                            //respond = "disconnect";
-                            while( respond.equals("disconnect") != true) {
-                                if (respond.equals("takeId")) {
-                                    System.out.println("взял");
-                                    ArrayList<Integer> verifiedId = rW.readVerifiedId();
-                                    for (int b : verifiedId
-                                    ) {
-                                        a.insertKey(b, Gen.genKey());
+                    RW rW = new RW(server);
+                    new Thread(()->{
+                        int Id;
+                        int key;
+                        DBAccess a;
+                       String gay = rW.readLine();
+                        switch (gay) {
+//Registration Android Client
+                            case "userRegData" :
+                                a = new DBAccess();
+                                Object[] userData = rW.readUserData();
+                                System.out.println("connected");
+                                a.setUserData((String) userData[0],(String)userData[1], (int)userData[2]);
+                                System.out.println(a.getId((String) userData[0],(String) userData[1],(int)userData[2]));
+                                //добавить инд ключ для id
+                                rW.write(a.getId((String) userData[0],(String) userData[1],(int)userData[2]));
+
+                                break;
+//Adding key to Android Client after verification
+                            case "userId":
+                                Id = rW.read();
+                                a = new DBAccess();
+                                key = a.checkKey(Id);
+                                if (key != 0) {
+
+                                    rW.write(key);
+                                }
+                                else {
+                                    try {
+                                        rW.close();
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
                                     }
                                 }
-                                else if (respond.equals("refresh")) {
-                                    System.out.println("рефрешнул");
-                                     ID = a.getUnverifiedId();
-                                     DATA =  a.getUnverifiedData();
-                                    rW.writeUnverified(ID,DATA);
+                                break;
+                            //Verify Android Client
+                            case "adminId":
+                                System.out.println("connected");
+                                a = new DBAccess();
+                                ArrayList<Integer> ID = a.getUnverifiedId();
+                                ArrayList<String> DATA =  a.getUnverifiedData();
+                                rW.writeUnverified(ID,DATA);
+                                String respond = rW.readLine();
+                                System.out.println(respond);
+
+                                //respond = "disconnect";
+                                while( respond.equals("disconnect") != true) {
+                                    if (respond.equals("takeId")) {
+                                        System.out.println("взял");
+                                        ArrayList<Integer> verifiedId = rW.readVerifiedId();
+                                        for (int b : verifiedId
+                                        ) {
+                                            a.insertKey(b, Gen.genKey());
+                                        }
+                                    }
+                                    else if (respond.equals("refresh")) {
+                                        System.out.println("рефрешнул");
+                                        ID = a.getUnverifiedId();
+                                        DATA =  a.getUnverifiedData();
+                                        rW.writeUnverified(ID,DATA);
+                                    }
+                                    respond = rW.readLine();
+
                                 }
-                                respond = rW.readLine();
-
-                            }
 
 
-                            break;
+                                break;
 
 
-                        case "userHelpRequest":
-                            String[] help = rW.readHelp();
-                            rW.writeLine("Help is incoming");
+                            case "userHelpRequest":
+                                String[] help = rW.readHelp();
+                                rW.writeLine("Help is incoming");
 //Attaching help[] request to the id the DB
-                            break;
-                        case  "socId":
+                                break;
+                            case  "socId":
 //If we got help request in DB push it to the soc client
 //Will add logic later
-                            break;
+                                break;
 //Raspberry asking for a key check
-                        case "userKey":
-                            key = rW.read();
+                            case "userKey":
+                                key = rW.read();
 //compare it to the key in DB
 //if true
-                            rW.writeLine("On");
+                                rW.writeLine("On");
 //If false
-                            rW.writeLine("Off");
-                            break;
+                                rW.writeLine("Off");
+                                break;
 
-                    }
+                        }
 
-                } catch (NullPointerException e) {
-                    e.printStackTrace();
-                }
+                    }).start();
+
+
+
             }
         }
 
